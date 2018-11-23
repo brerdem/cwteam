61f187aeb4e5c7bf518842c3df157d7ad87a9a6a
Burak.Erdem <burak.erdem@clockwork.com.tr>
2018-11-23 15:47:22 +0300

    init

---
+++ client/src/pages/Projects.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+class Projects extends Component {
+
+
+    render() {
+        return (
+            <div>
+               <span>These are projects</span>
+            </div>
+        );
+    }
+}
+
+export default Projects;
