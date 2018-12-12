cf196a5c5a1443b4b39f36a81f64f944f70695cb
Burak.Erdem <burak.erdem@clockwork.com.tr>
2018-12-12 19:56:09 +0300

    some timeline functions added

--- client/src/components/todo/Timeline.js
+++ client/src/components/todo/Timeline.js
@@ -10,44 +10,49 @@ import Grid from "@material-ui/core/es/Grid/Grid";
 function makeDataForGantt(projects) {
     let data = {};
     data.data = [];
-    projects.map((project, index) => {
+    projects.forEach((project, index) => {
+
         data.data.push(
-            {id: `${index}_0`, text: project.name, open: true, color:'#2b8496', holder:''},
+            {id: `${index}_0`, text: project.name, open: true, color:'#2b8496', holder:'', progress: 1.9/4, editable: false, task_type: 'project'},
             {
                 id: `${index}_1`,
                 text: initialData.tasks["task-1"].content,
                 start_date: '14-12-2018',
                 duration: 2,
-                holder: 'Burak Erdem',
+                holder: 'Barış K.',
                 parent: `${index}_0`,
                 color: initialData.tasks["task-1"].categoryColor,
+                progress: 0.4
             },
             {
                 id: `${index}_2`,
                 text: initialData.tasks["task-2"].content,
                 start_date: '16-12-2018',
                 duration: 3,
-                holder: 'Burak Erdem',
+                holder: 'Çağla K.',
                 parent: `${index}_0`,
                 color: initialData.tasks["task-2"].categoryColor,
+                progress: 0.5
             },
             {
                 id: `${index}_3`,
                 text: initialData.tasks["task-3"].content,
                 start_date: '18-12-2018',
                 duration: 4,
-                holder: 'Burak Erdem',
+                holder: 'Burak E.',
                 parent: `${index}_0`,
                 color: initialData.tasks["task-3"].categoryColor,
+                progress: 0.2
             },
             {
                 id: `${index}_4`,
                 text: initialData.tasks["task-4"].content,
                 start_date: '21-12-2018',
                 duration: 4,
-                holder: 'Burak Erdem',
+                holder: 'Deniz Z.',
                 parent: `${index}_0`,
                 color: initialData.tasks["task-4"].categoryColor,
+                progress: 0.8
             },
         )
     });
--- client/src/components/todo/timeline/Gantt.js
+++ client/src/components/todo/timeline/Gantt.js
@@ -1,17 +1,60 @@
 /*global gantt*/
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import 'dhtmlx-gantt';
 import 'dhtmlx-gantt/codebase/dhtmlxgantt.css';
 import 'dhtmlx-gantt/codebase/locale/locale_tr'
 
+
+function setTitleGridRow(task) {
+    if (task.task_type) {
+        return "<b style='color:#3F51B5'>"+task.text+"</b>";
+    }
+    return task.text;
+}
+
+
 export default class Gantt extends Component {
+
     componentDidMount() {
-        gantt.config.columns =  [
-            {name:"text",       label:"Görev Adı",  tree:true, width:250 },
-            {name:"start_date", label:"Başlangıç", align:"center" },
-            {name:"holder",     label:"Sorumlu",     align:"center" },
-            {name:"duration",   label:"Süre",   align:"center" },
+
+
+
+        gantt.config.columns = [
+            {name: "text", label: "Görev Adı", tree: true, width: 300, template: setTitleGridRow},
+            {name: "start_date", label: "Başlangıç", align: "center", width: 100},
+            {name: "holder", label: "Sorumlu", align: "center", width: 100},
+            {name: "duration", label: "Süre", align: "center", width: 40},
         ];
+
+
+
+        gantt.config.sort = true;
+        gantt.templates.leftside_text = function (start, end, task) {
+            if (task.task_type) {
+                return "<b style='font-size:14px'>%" + Math.floor(task.progress * 100) + "</b>";
+            }
+            return "<b>%" + Math.floor(task.progress * 100) + "</b>";
+        };
+
+        gantt.attachEvent("onAfterTaskUpdate", function (id, item) {
+            const parent = item.parent;
+            let children = gantt.getChildren(parent);
+            let sum = 0;
+            children.forEach((child) => {
+                sum += gantt.getTask(child).progress;
+            });
+
+            gantt.getTask(parent).progress = sum / children.length;
+            gantt.refreshData();
+        });
+
+        gantt.templates.task_class = function(start, end, task){
+            let css = [];
+            if(task.task_type === 'project'){
+                css.push("no_drag_progress");
+            }
+            return css.join(" ");
+        }
 
 
         gantt.init(this.ganttContainer);
@@ -21,7 +64,9 @@ export default class Gantt extends Component {
     render() {
         return (
             <div
-                ref={(input) => { this.ganttContainer = input }}
+                ref={(input) => {
+                    this.ganttContainer = input
+                }}
                 style={{width: '100%', height: '100%'}}
             ></div>
         );
